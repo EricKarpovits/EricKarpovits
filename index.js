bc8407fe60d0ae9a9ab226d47f274fd473e97523
Eric Karpovits <eric162003@gmail.com>
2021-03-14 03:55:55 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,19 +1,12 @@
-// index.js
-require('dotenv').config(); 
+require('dotenv').config();
 const Mustache = require('mustache');
 const fetch = require('node-fetch');
 const fs = require('fs');
+const puppeteerService = require('./services/puppeteer.service');
 
 const MUSTACHE_MAIN_DIR = './main.mustache';
 
-const puppeteerService = require('./services/puppeteer.service');
-/**
-  * DATA is the object that contains all
-  * the data to be provided to Mustache
-  * Notice the "name" and "date" property.
-*/
 let DATA = {
-  name: 'Eric Karpovits',
   refresh_date: new Date().toLocaleDateString('en-US', {
     weekday: 'long',
     month: 'long',
@@ -21,47 +14,41 @@ let DATA = {
     hour: 'numeric',
     minute: 'numeric',
     timeZoneName: 'short',
-    timeZone: 'America/Toronto', 
+    timeZone: 'America/Toronto',
   }),
 };
 
 async function setWeatherInformation() {
-    await fetch(
-      `https://api.openweathermap.org/data/2.5/weather?q=stockholm&appid=${process.env.OPEN_WEATHER_MAP_KEY}&units=metric`
-    /*  `http://api.openweathermap.org/data/2.5/weather?id=6094817&appid=${process.env.OPEN_WEATHER_MAP_KEY}&units=metric`*/
-    )
-      .then(r => r.json())
-      .then(r => {
-        DATA.city_temperature = Math.round(r.main.temp);
-        DATA.city_weather = r.weather[0].description;
-        DATA.city_weather_icon = r.weather[0].icon;
-        DATA.sun_rise = new Date(r.sys.sunrise * 1000).toLocaleString('en-US', {
-          hour: '2-digit',
-          minute: '2-digit',
-          timeZone: 'America/Toronto',
-        });
-        DATA.sun_set = new Date(r.sys.sunset * 1000).toLocaleString('en-US', {
-          hour: '2-digit',
-          minute: '2-digit',
-          timeZone: 'America/Toronto',
-        });
+  await fetch(
+    `https://api.openweathermap.org/data/2.5/weather?q=stockholm&appid=${process.env.OPEN_WEATHER_MAP_KEY}&units=metric`
+  )
+    .then(r => r.json())
+    .then(r => {
+      DATA.city_temperature = Math.round(r.main.temp);
+      DATA.city_weather = r.weather[0].description;
+      DATA.city_weather_icon = r.weather[0].icon;
+      DATA.sun_rise = new Date(r.sys.sunrise * 1000).toLocaleString('en-GB', {
+        hour: '2-digit',
+        minute: '2-digit',
+        timeZone: 'Europe/Stockholm',
       });
+      DATA.sun_set = new Date(r.sys.sunset * 1000).toLocaleString('en-GB', {
+        hour: '2-digit',
+        minute: '2-digit',
+        timeZone: 'Europe/Stockholm',
+      });
+    });
 }
 
 async function setInstagramPosts() {
-  const instagramImages = await puppeteerService.getLatestInstagramPostsFromAccount('ottawatourism', 3);
+  const instagramImages = await puppeteerService.getLatestInstagramPostsFromAccount('visitstockholm', 3);
   DATA.img1 = instagramImages[0];
   DATA.img2 = instagramImages[1];
   DATA.img3 = instagramImages[2];
 }
 
-/**
-  * A - We open 'main.mustache'
-  * B - We ask Mustache to render our file with the data
-  * C - We create a README.md file with the generated output
-  */
-function generateReadMe() {
-  fs.readFile(MUSTACHE_MAIN_DIR, (err, data) =>  {
+async function generateReadMe() {
+  await fs.readFile(MUSTACHE_MAIN_DIR, (err, data) => {
     if (err) throw err;
     const output = Mustache.render(data.toString(), DATA);
     fs.writeFileSync('README.md', output);
@@ -75,8 +62,8 @@ async function action() {
   await setWeatherInformation();
 
   /**
-   * Get pictures
-   */
+  * Get pictures
+  */
   await setInstagramPosts();
 
   /**
